e6e836567b92746353f51d0f3a73ef60880574c8
m0ar <edvard@hubinette.me>
2024-07-09 11:57:11 +0200

    Add dunst config

--- args/dunst/default.nix
+++ args/dunst/default.nix
@@ -1,5 +1,32 @@
-{ ... }: {
-  configFiles = {
-    "dunst/dunstrc".source = ./dunstrc;
+{ pkgs, ... }: {
+  iconTheme = {
+    name = "Papirus-Dark";
+    package = pkgs.papirus-nord;
+    size = "32x32";
+  };
+  settings = {
+    global = {
+      offset = "100x100";
+      font = "FiraCode 12";
+      # frame_color = "#90648b";
+    };
+
+    urgency_low = {
+      background = "#282A2E";
+      foreground = "#6272a4";
+      timeout = 10;
+    };
+
+    urgency_normal = {
+      background = "#282A2E";
+      foreground = "#bd93f9";
+      timeout = 10;
+    };
+    
+    urgency_critical = {
+      background = "#282A2E";
+      foreground = "#f8f8f2";
+      timeout = 10;
+    };
   };
 }
